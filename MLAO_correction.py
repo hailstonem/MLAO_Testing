29ac2776bd2aede711f066f748b1a3abd7e5e1e0
M <hailstonem@users.noreply.github.com>
2020-09-23 14:04:41 +0100

    Correct for negative input for 'after' image as well

--- MLAO_correction.py
+++ MLAO_correction.py
@@ -198,7 +198,7 @@ def ML_estimate(iterative_correct, scan):
             image = capture_image(scanner)
 
             tifffile.imsave(
-                "./results/%s_%s_after_%s.tif" % (rnd, mode, it + 1), image.astype('float32')/(image.max())
+                "./results/%s_%s_after_%s.tif" % (rnd, mode, it + 1), -image.astype('float32')/(image.min())
             )  # rnd just there to make overwrites unlikely. Replace with proper solution when we have a better idea of what we want to save
 
             print("Mode " + str(mode) + " Applied = " + str(1))

68cf85b1c0579465e6c0ba93f9b8a8ef1f5e60ed
M <hailstonem@users.noreply.github.com>
2020-12-07 10:15:45 +0000

    Polynomial estimate: clearer aberration tracking

--- MLAO_correction.py
+++ MLAO_correction.py
@@ -73,7 +73,24 @@ def set_slm_and_capture_image(scanner, image_dim, aberration, aberration_modes, 
     return image
 
 
-def polynomial_estimate(bias_modes, bias_magnitude, params):
+class AberrationHistory:
+    def __init__(self, initial_aberration):
+        self.aberration = [np.array(initial_aberration)]
+        self.prediction = []
+
+    def update(self, aberration=None, prediction=None):
+        if aberration is not None and prediction is not None:
+            self.aberration.append(aberration)
+            self.prediction.append(prediction)
+        elif aberration is not None:
+            self.aberration.append(aberration)
+            self.prediction.append(self.aberration[-1] - self.aberration[-2])
+        elif prediction is not None:
+            self.prediction.append(prediction)
+            self.aberration.append(self.aberration[-1] - prediction)
+
+
+def polynomial_estimate(bias_modes, return_modes, bias_magnitude, params):
     """Performs correction based on 3n polynomial fitting. Same API as ml_estimate, but also requires bias_modes parameter"""
     iterations = params.iter
 
@@ -97,15 +114,20 @@ def polynomial_estimate(bias_modes, bias_magnitude, params):
     for mode in scan_modes:
 
         start_aberrations = np.zeros((max(bias_modes) + 1))
+        jsonfile = folder + "/%03d_%s_coefficients.json" % (rnd, mode,)
+        tifname = folder + "/%03d_%s_iterations.tif" % (rnd, mode)
+
         if params.load_abb:
             start_aberrations = load_start_abb("./start_abb.json", start_aberrations)
             log.debug("abberation loaded")
 
         if mode:
             start_aberrations[mode] += params.magnitude
-
+        tracked_aberrations = start_aberrations[:]
+        abb_his = AberrationHistory(start_aberrations)
         for it in range(iterations + 1):
-            log.info(f"it {it} coefficients:{[np.round(a, 1) for a in start_aberrations]}")
+            log.info(f"it {it} coefficients:{[np.round(a, 1) for a in tracked_aberrations]}")
+
             # Set up scan
             image_dim = (128, 128)  # set as appropriate
             scanner.SetScanPixelRange(ScannerPixelRange(x=image_dim[1] + 2, y=image_dim[0] + 2))
@@ -113,16 +135,17 @@ def polynomial_estimate(bias_modes, bias_magnitude, params):
             for bn, bias in enumerate(bias_modes):
                 # Get lists of biases and aberrations
                 list_of_aberrations_lists = make_bias_polytope(
-                    start_aberrations, [bias], max(bias_modes) + 1, steps=[bias_magnitude]
+                    tracked_aberrations, [bias], max(bias_modes) + 1, steps=[bias_magnitude]
                 )
-                aberration_modes = [int(i) for i in range(len(start_aberrations))]
-                stack = np.zeros((image_dim[0], image_dim[1], len(list_of_aberrations_lists)))
+                aberration_modes = [int(i) for i in range(len(tracked_aberrations))]
+
                 # randomize image collection to minimise effects of bleaching
                 shuffled_order = np.arange(len(list_of_aberrations_lists))
                 if params.shuffle:
                     np.random.shuffle(shuffled_order)
 
                 # Get stack of images
+                stack = np.zeros((image_dim[0], image_dim[1], len(list_of_aberrations_lists)))
                 for i_image in shuffled_order:
                     aberration = list_of_aberrations_lists[i_image]
 
@@ -137,34 +160,51 @@ def polynomial_estimate(bias_modes, bias_magnitude, params):
 
                 # format
                 stack = np.rollaxis(stack, 2, 0)
+
                 # calculate metric
                 intensities = Intensity_Metric(stack, stack.shape[1] // 2, params.centerrange)
                 coeffarray = [x[bias] for x in list_of_aberrations_lists]
                 optimal = optimisation(coeffarray, intensities, degree_fitting=2)
-                pred[bias] = optimal - start_aberrations[bias]
-                start_aberrations[bias] = optimal  # use this as starting point for next correction
+                log.debug(f"Mode {bias} Fit: {optimal}")
+
                 # save tif and brightness from image before correction
                 if bn == 0:
                     log.debug("saving tif")
                     save_tif(tifname, stack[0, :, :].astype("float32"))
                     brightness = np.mean(stack[0])
+                # use this as starting point for next correction
+                tracked_aberrations[bias] = optimal
+            abb_his.update(aberration=tracked_aberrations)
 
             # save to json and tif
-            jsonfile = folder + "/%03d_%s_coefficients.json" % (rnd, mode,)
+            coeff_to_json(
+                jsonfile,
+                abb_his.aberration[it],
+                bias_modes,
+                abb_his.prediction[it][bias_modes],
+                it + 1,
+                brightness=brightness,
+            )
             # if not params.correct_bias_only:
             #    coeff_to_json(jsonfile, start_aberrations, return_modes, pred, it + 1)
             # else:
-            coeff_to_json(
-                jsonfile,
-                tuple(start_aberrations),
-                bias_modes,
-                pred[bias_modes],
-                it + 1,
-                brightness=np.mean(stack[0]),
-            )
+            if it == iterations:
 
-            tifname = folder + "/%03d_%s_iterations.tif" % (rnd, mode)
-            save_tif(tifname, stack[0, :, :].astype("float32"))  # /stack[0, :, :, 0].max())
+                list_of_aberrations_lists = make_bias_polytope(
+                    tracked_aberrations, bias_modes, max(bias_modes) + 1, steps=[]
+                )
+                image = set_slm_and_capture_image(
+                    scanner, image_dim, list_of_aberrations_lists[0], aberration_modes, 1
+                )
+                save_tif(tifname, image.astype("float32"))
+                coeff_to_json(
+                    jsonfile,
+                    tuple(tracked_aberrations),
+                    bias_modes,
+                    np.zeros_like(pred)[bias_modes],
+                    it + 2,
+                    brightness=np.mean(image),
+                )
         jsonfilelist.append((jsonfile, "%03d_%s" % (rnd, mode)))
     return jsonfilelist
 
